6caa1b9fdac34d85080e4aa1bbd401a4e3abad40
safi miazi <mohibullamiazi@gmail.com>
2025-01-01 23:17:56 +0600

    added achivement section

--- src/components/ui/home/Achivement.tsx
+++ src/components/ui/home/Achivement.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import ContentWidth from "@/components/shared/ContentWidth";
+import React from "react";
 
 const Achivement = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="py-20">
+      <ContentWidth>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-3 items-center justify-between">
+          {/* card 1 */}
+          <div></div>
         </div>
-    );
+      </ContentWidth>
+    </div>
+  );
 };
 
 export default Achivement;
