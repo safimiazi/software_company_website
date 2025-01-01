1cf6cb17c1543bb5548190ff510c3ce03e72bfae
safi miazi <mohibullamiazi@gmail.com>
2025-01-02 02:04:09 +0600

    add animation of the case studies secion of home page

--- src/components/ui/home/CaseStudies.tsx
+++ src/components/ui/home/CaseStudies.tsx
@@ -58,38 +58,42 @@ const CaseStudies = () => {
         {/* Service Cards */}
         <div className="grid md:grid-cols-4 grid-cols-1 gap-5">
           {serviceSectionData.services.map((service, inx) => (
-           <div key={inx} className="relative rounded-md group z-30 card-container">
-           <Image
-             src={Case}
-             alt="About Us"
-             width={1000}
-             height={1000}
-             className="w-full h-full"
-           />
-           {/* Background Gradient */}
-           <div className="card-background"></div>
-         
-           {/* Content Section */}
-           <div className="absolute border-t-4 border-t-brand-color bg-black z-50 group-hover:-translate-y-0 duration-300 translate-y-4 bottom-0 p-2 rounded-t-md w-9/12 max-h-fit left-1/2 transform -translate-x-1/2 text-white">
-             <div className="flex items-center gap-2">
-               <span className="w-2 h-2 bg-red-500 rounded-full"></span>
-               <span className="text-red-500 text-sm font-semibold">Company</span>
-             </div>
-             <h3 className="text-lg font-bold mt-2">
-               Corporate Finance for helpfulness Markets
-             </h3>
-             <Link
-               href={""}
-               className="text-sm opacity-0 group-hover:opacity-100 duration-300 font-medium mt-4 inline-flex items-center gap-2 text-gray-300 hover:text-white transition"
-             >
-               Read More <span className="text-lg">+</span>
-             </Link>
-           </div>
-         
-           {/* Border Section */}
-           <div className="absolute -bottom-5 -z-30 group-hover:border-brand-color transition-all duration-300 w-11/12 h-full left-1/2 transform -translate-x-1/2 border-b border-l border-r border-gray-400 border-opacity-40 overflow-hidden"></div>
-         </div>
-         
+            <div
+              key={inx}
+              className="relative rounded-md group z-30 card-container"
+            >
+              <Image
+                src={Case}
+                alt="About Us"
+                width={1000}
+                height={1000}
+                className="w-full h-full"
+              />
+              {/* Background Gradient */}
+              <div className="card-background"></div>
+
+              {/* Content Section */}
+              <div className="absolute border-t-4 border-t-brand-color bg-black z-50 group-hover:-translate-y-0 duration-300 translate-y-4 bottom-0 p-2 rounded-t-md w-9/12 max-h-fit left-1/2 transform -translate-x-1/2 text-white">
+                <div className="flex items-center gap-2">
+                  <span className="w-2 h-2 bg-red-500 rounded-full"></span>
+                  <span className="text-red-500 text-sm font-semibold">
+                    Company
+                  </span>
+                </div>
+                <h3 className="text-lg font-bold mt-2">
+                  Corporate Finance for helpfulness Markets
+                </h3>
+                <Link
+                  href={""}
+                  className="text-sm opacity-0 group-hover:opacity-100 duration-300 font-medium mt-4 inline-flex items-center gap-2 text-gray-300 hover:text-white transition"
+                >
+                  Read More <span className="text-lg">+</span>
+                </Link>
+              </div>
+
+              {/* Border Section */}
+              <div className="absolute -bottom-5 -z-30 group-hover:border-brand-color transition-all duration-300 w-11/12 h-full left-1/2 transform -translate-x-1/2 border-b border-l border-r border-gray-400 border-opacity-40 overflow-hidden"></div>
+            </div>
           ))}
         </div>
       </ContentWidth>
