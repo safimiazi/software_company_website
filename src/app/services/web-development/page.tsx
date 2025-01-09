caa0091a40dffce7c10fc9a25c3db7201d079eac
safi miazi <mohibullamiazi@gmail.com>
2025-01-09 21:23:25 +0600

    added component to the pages

--- src/app/services/web-development/page.tsx
+++ src/app/services/web-development/page.tsx
@@ -1,4 +1,5 @@
 import Client_Testimonials from "@/components/shared/common/Client_Testimonials";
+import Why_choose_us from "@/components/shared/common/Why_choose_us";
 import About_web_development from "@/components/ui/website_development/About_web_development";
 import Benefits_web_development from "@/components/ui/website_development/Benefits_web_development";
 import Portfolio_web_development from "@/components/ui/website_development/Portfolio_web_development";
@@ -9,15 +10,14 @@ import React from "react";
 const WebDevelopment = () => {
   return (
     <div className="">
-     
-
       {/* Banner Content */}
       <Web_development_banner />
-      <About_web_development/>
-      <Service_web_development/>
-      <Benefits_web_development/>
-      <Portfolio_web_development/>
-      <Client_Testimonials/>
+      <About_web_development />
+      <Service_web_development />
+      <Benefits_web_development />
+      <Portfolio_web_development />
+      <Client_Testimonials />
+      <Why_choose_us />
     </div>
   );
 };
