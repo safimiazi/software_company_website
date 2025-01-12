0fb917d4228de914fd027d92a076ee91492785e4
safi miazi <mohibullamiazi@gmail.com>
2025-01-12 20:22:05 +0600

    update

--- src/app/about-us/company-overview/page.tsx
+++ src/app/about-us/company-overview/page.tsx
@@ -1,3 +1,6 @@
+import Client_Testimonials from "@/components/shared/common/Client_Testimonials";
+import Portfolio from "@/components/shared/common/Portfolio";
+import Why_choose_us from "@/components/shared/common/Why_choose_us";
 import Company_overview_about from "@/components/ui/company_overview/Company_overview_about";
 import Company_overview_banner from "@/components/ui/company_overview/Company_overview_banner";
 import Company_overview_services from "@/components/ui/company_overview/Company_overview_services";
@@ -9,6 +12,9 @@ const CompanyOverview = () => {
       <Company_overview_banner />
       <Company_overview_about />
       <Company_overview_services/>
+      <Why_choose_us/>
+      <Portfolio/>
+      <Client_Testimonials/>
     </div>
   );
 };
--- src/components/ui/company_overview/Company_overview_banner.tsx
+++ src/components/ui/company_overview/Company_overview_banner.tsx
@@ -1,24 +1,26 @@
-import ContentWidth from '@/components/shared/ContentWidth';
 import React from 'react';
 
-const Company_overview_banner: React.FC = () => {
+const CompanyOverviewBanner: React.FC = () => {
     return (
         <div
             className="relative bg-cover bg-center h-[400px] flex items-center"
             style={{ backgroundImage: `url('/Company_overview_banner.jpg')` }}
         >
+            {/* Overlay */}
             <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-            <div className="relative z-10 max-w-3xl ml-10 text-white">
-                <ContentWidth>
-                <h1 className="text-4xl font-bold mb-4">Welcome to Our Company</h1>
-                <p className="text-lg max-w-[600px]">
-                    At Easy Soft Bangladesh, we empower businesses with innovative software solutions tailored to their
-                    needs.
-                </p>
-                </ContentWidth>
+
+            {/* Content */}
+            <div className="relative z-10 w-full max-w-7xl mx-auto px-6 text-white">
+                <div className="max-w-3xl">
+                    <h1 className="text-4xl font-bold mb-4">Welcome to Our Company</h1>
+                    <p className="text-lg max-w-[600px]">
+                        At Easy Soft Bangladesh, we empower businesses with innovative software solutions tailored to their
+                        needs.
+                    </p>
+                </div>
             </div>
         </div>
     );
 };
 
-export default Company_overview_banner;
+export default CompanyOverviewBanner;
