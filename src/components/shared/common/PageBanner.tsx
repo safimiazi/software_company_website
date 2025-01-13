be5d35ef94ba2d79703175a9c65ff12ffe8982c2
safi miazi <mohibullamiazi@gmail.com>
2025-01-13 22:08:57 +0600

    update

--- src/components/shared/common/PageBanner.tsx
+++ src/components/shared/common/PageBanner.tsx
@@ -1,30 +1,49 @@
-import React from 'react';
+import { PlusIcon } from "@/Icons";
+import Link from "next/link";
+import React from "react";
 
 interface BannerProps {
-    backgroundImage: string;
-    title: string;
-    description: string;
-    cta?: string;
+  backgroundImage: string;
+  title: string;
+  description: string;
+  cta?: string;
+  cta_link?: any;
 }
 
-const PageBanner: React.FC<BannerProps> = ({ backgroundImage, title, description }) => {
-    return (
-        <div
-            className="relative bg-cover bg-center h-[400px] flex items-center"
-            style={{ backgroundImage: `url('${backgroundImage}')` }}
-        >
-            {/* Overlay */}
-            <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+const PageBanner: React.FC<BannerProps> = ({
+  backgroundImage,
+  title,
+  description,
+  cta,
+  cta_link,
+}) => {
+  return (
+    <div
+      className="relative bg-cover bg-center h-[400px] flex items-center"
+      style={{ backgroundImage: `url('${backgroundImage}')` }}
+    >
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
-            {/* Content */}
-            <div className="relative z-10 w-full max-w-7xl mx-auto px-6 text-white">
-                <div className="max-w-3xl">
-                    <h1 className="text-4xl font-bold mb-4">{title}</h1>
-                    <p className="text-lg max-w-[600px]">{description}</p>
-                </div>
-            </div>
+      {/* Content */}
+      <div className="relative z-10 w-full max-w-7xl mx-auto px-6 text-white">
+        <div className="max-w-3xl">
+          <h1 className="text-4xl font-bold mb-4">{title}</h1>
+          <p className="text-lg max-w-[600px] mb-4">{description}</p>
+          {cta && cta_link && (
+            <Link href={`${cta_link}`}>
+              <button className="flex items-center justify-between bg-brand-color rounded-full p-3 gap-2 text-white">
+                {cta}
+                <span className="p-1 bg-white rounded-full text-brand-color">
+                  <PlusIcon />
+                </span>
+              </button>
+            </Link>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PageBanner;
--- src/components/ui/career/Career_banner.tsx
+++ src/components/ui/career/Career_banner.tsx
@@ -8,6 +8,9 @@ const Career_banner = () => {
         backgroundImage="/Company_overview_banner.jpg"
         title="Build Your Future with Us"
         description="Join Easy Soft Bangladesh to innovate, grow, and make an impact in the digital world. Your journey to excellence starts here."
+        cta="Explore Open Positions"
+        cta_link="#"
+      
       />
     </div>
   );
