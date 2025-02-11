75f0feaf99b37fcdb3ff0452fef49fade1df646f
safi miazi <mohibullamiazi@gmail.com>
2025-02-11 12:22:16 +0600

    refactor: remove unused API endpoints from Proxy component

--- src/Proxy.tsx
+++
@@ -1,7 +0,0 @@
-// home banner
-export const get_home_page_banner_data_api_url="http://localhost:5000/api/v1/home_banner/get_home_page_banner_data"
-export const home_banner_image_api = "http://localhost:5000/api/v1/home_banner/get_home_page_banner_images"
-
-// home about
-export const get_home_page_about_data_api_url="http://localhost:5000/api/v1/home_about/get_home_page_about_data"
-export const home_about_image_api = "http://localhost:5000/api/v1/home_about/get_home_page_about_images"
